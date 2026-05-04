945a6868916d00688298523ff3654a9711064068
StarPlatinum2283334 <egor_maksovich@mail.ru>
2026-05-04 15:02:05 +0700

    Update README.js

--- README.js
+++ README.js
@@ -1,16 +1,37 @@
-function generateRandomPassword(length = 12) {
-    const lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numbers = "0123456789";
-    const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
-
-    const allChars = lowercase + uppercase + numbers + symbols;
-
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * allChars.length);
-        password += allChars[randomIndex];
+// Генерация случайного пароля из выбранных символов (криптостойкая)
+    function generateRandomPassword() {
+        const length = parseInt(passwordLengthSlider.value);
+        let passwordChars = [];
+        
+        const sets = [];
+        if (lowercaseCheck.checked) sets.push('abcdefghijklmnopqrstuvwxyz');
+        if (uppercaseCheck.checked) sets.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        if (numbersCheck.checked) sets.push('0123456789');
+        if (symbolsCheck.checked) sets.push('!@#$%^&*()_+-=[]{};:,.<>?');
+        
+        if (sets.length === 0) {
+            sets.push('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*');
+        }
+        
+        sets.forEach(set => {
+            passwordChars.push(secureRandomChar(set));
+        });
+        
+        const allChars = sets.join('');
+        while (passwordChars.length < length) {
+            passwordChars.push(secureRandomChar(allChars));
+        }
+        
+        for (let i = passwordChars.length - 1; i > 0; i--) {
+            const j = secureRandomInt(i + 1);
+            [passwordChars[i], passwordChars[j]] = [passwordChars[j], passwordChars[i]];
+        }
+        
+        return passwordChars.join('');
     }
-
-    return password;
-}
+    
+    function handleGenerate() {
+        const newPassword = generateRandomPassword();
+        generatedPasswordSpan.textContent = newPassword;
+        generatedContainer.style.display = 'block';
+    }
